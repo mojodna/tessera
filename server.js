b03e27d8c96c7123b5cc074c917645e0a9e8eff3
Seth Fitzsimmons <seth@mojodna.net>
2014-01-26 16:41:16 -0800

    Experimentation with npm-based module discovery

--- server.js
+++ server.js
@@ -6,9 +6,58 @@ process.env.UV_THREADPOOL_SIZE = process.env.UV_THREADPOOL_SIZE || Math.ceil(Mat
 
 var cors = require("cors"),
     express = require("express"),
+    npm = require("npm"),
     tilelive = require("tilelive-cache")(require("tilelive"), {
       size: process.env.CACHE_SIZE || 10
     });
+
+npm.load(function() {
+  // TODO tilelive-* should have tilelive as a peer dependency
+  // TODO mbtiles won't be detected
+  // TODO need a way to distinguish between calling to register and using
+  // registerProtocols
+  var extensions = {};
+
+  npm.commands.ls(null, true, function(err, packages) {
+    var names = Object.keys(packages.dependencies)
+      .filter(function(name) {
+        // TODO check for a tilelive key
+        return name.match(/^tilelive-/);
+      })
+      .map(function(name) {
+        extensions[name] = true;
+        return name;
+      });
+
+    console.log();
+    console.log("local dependencies:");
+    console.log(names.join("\n"));
+
+    npm.config.set("global", true);
+    npm.commands.ls(null, true, function(err, packages) {
+      npm.config.set("global", false);
+
+      var names = Object.keys(packages.dependencies)
+        .filter(function(name) {
+          return name.match(/^tilelive-/);
+        })
+        .map(function(name) {
+          extensions[name] = true;
+          return name;
+        });
+
+      // TODO don't re-require modules that have already been found
+
+      console.log();
+      console.log("global dependencies:");
+      console.log(names.join("\n"));
+
+      console.log();
+      console.log("to load:");
+      console.log(Object.keys(extensions).join("\n"));
+    });
+  });
+});
 
 try { require("tilejson").registerProtocols(tilelive); } catch (e) {}
 try { require("tilelive-bridge").registerProtocols(tilelive); } catch (e) {}
