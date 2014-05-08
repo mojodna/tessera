4932ee0931185a673a8aa9d3b3e35e1b9fe79686
Seth Fitzsimmons <seth@mojodna.net>
2014-05-07 17:07:00 -0700

    Upgrade tilelive-http dependency

--- modules.js
+++ modules.js
@@ -5,7 +5,7 @@ module.exports = function(tilelive) {
   try { require("tilelive-bridge").registerProtocols(tilelive); } catch (e) {}
   try { require("tilelive-carto")(tilelive); } catch (e) {}
   try { require("tilelive-file").registerProtocols(tilelive); } catch (e) {}
-  try { require("tilelive-http").registerProtocols(tilelive); } catch (e) {}
+  try { require("tilelive-http")(tilelive); } catch (e) {}
   try { require("tilelive-mapbox")(tilelive); } catch (e) {}
   try { require("tilelive-mapnik").registerProtocols(tilelive); } catch (e) {}
   try { require("tilelive-tmsource")(tilelive); } catch (e) {}
